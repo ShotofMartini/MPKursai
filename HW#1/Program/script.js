6eccf9e02d036b669f83124672c82094f3799ea2
Martynas <martynas.simniskis@gmail.com>
2025-02-27 21:21:27 +0200

    pakeisti item buttons; galima unchekinti itemus, pradejo veikti saugojimas taciau dar yra bugu ir nesaugo statuso ar yra uzbrauktas

--- HW#1/Program/script.js
+++ HW#1/Program/script.js
@@ -1,98 +1,133 @@
+// Selectors
 const addButton = document.getElementById("add-button");
-const clearButton = document.getElementById("clear-button");
-const saveButton = document.getElementById("save-button");
 const emptyButton = document.getElementById("empty-button");
-addButton.addEventListener("click", addTask);
-document.addEventListener("keypress", enterPress);
 const input = document.getElementById("task");
 const toDoList = document.getElementById("todo-list");
-//new code
-//clearButton.addEventListener("click", clearCompletedToDoItems);
-checkedButton.addEventListener("click", toggleToDoItemState);
-saveButton.addEventListener("click", saveList);
-emptyButton.addEventListener("click", emptyList); //kazkodel neveikia nors funkcija veikia:(
-let addItemButton = document.getElementById('checkedButton');
-document.querySelector("span.save-button").addEventListener("click", saveList);
 
+// Event Listeners
+document.addEventListener("DOMContentLoaded", onLoad);
+addButton.addEventListener("click", addTask);
+document.addEventListener("keypress", enterPress);
+emptyButton.addEventListener("click", emptyList); //neveike, tada sutvarkiau komentarus ir perdeliojau ir volia veikia
+
+/**
+ * Checks if the input is valid and adds a new task to the list
+ */
 function addTask() {
-  /*alert("Add button clicked!");*/
   const itemText = input.value; //itemText priskiriamas - ivestas task
   if (itemText == ""){
     alert("No value!"); //patikrina ar ivestis ne tuscia
     return;
   }
-  console.log("Works");
-  console.log(input.value);
   newToDoItem(itemText); //kreipiasi i funkcija ir duoda ivesties texta
 }
+
+/**
+ * Creates new to do item in ul list 
+ */
 function newToDoItem(itemText) {
   const toDoItem = document.createElement("li"); //toDOItem bus li elementas
   toDoItem.className = "toDoItem";
-    
+  const p = document.createElement("span");
+  p.className = ("pText");
+
   let check = document.createElement("BUTTON"); //KAIP PADARYT ITEMA SU MYGTUKAIS???
   check.className = "buttonInList";
-  check.textContent = "Check";
-  
-  let remove = document.createElement('BUTTON'); //KAIP PADARYT ITEMA SU DVIEM MYGTUKAIS???
-  remove.textContent = "Remove";
+  check.innerHTML = "&#9989;";  // Shows the checkmark symbol ✔
+  check.id = "check-button";
+  check.addEventListener("click", function checkInside() {
+    toggleToDoItemState(p);
+  });
+
+  let remove = document.createElement('BUTTON');
+  remove.innerHTML = "&#10060;";
   remove.className = "buttonInList";
+  remove.id = "remove-button";
+  remove.addEventListener("click", function removeItemInside() {
+    removeItem(toDoItem);
+  });
   
-  toDoItem.textContent=itemText; //li elementui priskiria ivesties teksta
-  toDoItem.appendChild(check);
-  toDoItem.appendChild(remove);
+  const container = document.createElement("div");
+  container.className = "container";
+  toDoItem.appendChild(p);
+  container.appendChild(check);
+  container.appendChild(remove);
+  p.textContent=itemText; //li elementui priskiria ivesties teksta
+  toDoItem.appendChild(container);
   toDoList.appendChild(toDoItem); //i lista iterpia ivesties teksta kaip li elementa
   input.value="";
+  saveList();
 }
+
+/**
+ * ccepts a keyboard event and adds a task if enter is pressed
+ */
 function enterPress (event) {
   if (event.code != "Enter"){
   return;
   }
   addTask();
 }
-//new functions
-function clearCompletedToDoItems() {
-  var completedItems = toDoList.getElementsByClassName("completed");
 
-  while (completedItems.length > 0) {
-    completedItems.item(0).remove();
-  }
-}
-function toggleToDoItemState() {
-  if (this.classList.contains("completed")) {
-    this.classList.remove("completed");
-  } else {
-    this.classList.add("completed");
-  }
-}
+//NEW FUNCTIONS
+/**
+ * Empties the while list of todo items
+ */
 function emptyList() {
-  console.log("Works");
-  var toDoItems = toDoList.children;
-  while (toDoItems.length > 0) {
-    toDoItems.item(0).remove();
-  }
+  toDoList.innerHTML = "";
+  saveList();
 }
+
+function toggleToDoItemState(item) {
+  item.classList.toggle("crosed");
+}
+
+function removeItem(element) {
+  element.remove();
+  saveList();
+}
+
+/**
+ * Saves list to a memory
+ */
 function saveList() {
   var toDos = [];
 
   for (var i = 0; i < toDoList.children.length; i++) {
     var toDo = toDoList.children.item(i);
-
-    var toDoInfo = {
-        "task": toDo.innerText,
-        "completed": toDo.classList.contains("completed")
-    };
-
-    toDos.push(toDoInfo);
+    toDos.push(toDo.innerText);
 }
   localStorage.setItem("toDos", JSON.stringify(toDos));
 }
-function loadList() {
+
+/**
+ * Loads list from a memory
+ */
+function onLoad() {
   if (localStorage.getItem("toDos") != null) {
       var toDos = JSON.parse(localStorage.getItem("toDos"));
 
       for (var i = 0; i < toDos.length; i++) {
           var toDo = toDos[i];
-          newToDoItem(toDo.task, toDo.completed);
+          newToDoItem(toDo);
       }
   }
+}
+
+
+
+function toggleTask(id, pEl, button) {
+
+}
+
+function updateTask(id, completed) {
+//pakeist completed statusa local storage
+}
+
+function removeTask(id, toDoItem) {
+
+}
+
+function getLocalStorageItems() {
+
 }
